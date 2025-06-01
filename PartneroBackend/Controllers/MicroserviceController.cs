cfb01698fad0301bb578a4f870475ba49012bbd1
valtosw <volodijbessarabov@knu.ua>
2025-06-01 09:25:10 +0300

    Add MicroserviceController for business info handling

---
+++ PartneroBackend/Controllers/MicroserviceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PartneroBackend.Models.DTOs;
+using PartneroBackend.Models.InfoClasses;
+using PartneroBackend.Services;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PartneroBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MicroserviceController : ControllerBase
+    {
+        [HttpPost]
+        [Route("send-business-info")]
+        public async Task SendBusinessInfo([FromBody] BusinessInfoForMicroservice businessInfo)
+        {
+            await MicroserviceHandler.SendBusinessInfo(businessInfo);
+        }
+    }
+}
