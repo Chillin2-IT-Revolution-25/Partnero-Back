aa3a82022bc85dc220b06aabb7851d1351ca4022
valtosw <volodijbessarabov@knu.ua>
2025-06-01 04:02:17 +0300

    Add ApplicationUser class for user management

---
+++ PartneroBackend/Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDbGenericRepository.Attributes;
+using PartneroBackend.Models.InfoClasses;
+
+namespace PartneroBackend.Models
+{
+    [CollectionName("users")]
+    public class ApplicationUser : MongoIdentityUser<ObjectId>
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId BusinessInfoId { get; set; }
+
+        [BsonIgnore]
+        public BusinessInfo? BusinessInfo { get; set; }
+
+        public PersonalInfo PersonalInfo { get; set; } = new();
+        public bool ShowBusinessProfile { get; set; } = false;
+    }
+}
