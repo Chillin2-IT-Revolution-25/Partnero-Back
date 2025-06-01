7a87d51c96935e5cb40694c2379142873dbf604e
valtosw <volodijbessarabov@knu.ua>
2025-06-01 04:03:33 +0300

    Add BusinessInfo class for business-related data

---
+++ PartneroBackend/Models/InfoClasses/BusinessInfo.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PartneroBackend.Models.InfoClasses
+{
+    public enum CompanySize
+    {
+        Small,
+        Medium,
+        Large
+    }
+
+    public class BusinessInfo
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId UserId { get; set; }
+
+        public string BusinessName { get; set; }
+        public string Category { get; set; }
+        public LocationInfo? Location { get; set; }
+
+        public string? Description { get; set; }
+        public CompanySize? CompanySize { get; set; }
+        public int? FoundedYear { get; set; }
+
+        public List<string> BusinessImageUrls { get; set; } = [];
+        public SocialMediaInfo SocialMedia { get; set; } = new();
+    }
+}
