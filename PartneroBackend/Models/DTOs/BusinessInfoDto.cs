98d2385afbdd076f263a0f11cfabc5057a249ad4
valtosw <volodijbessarabov@knu.ua>
2025-06-01 09:24:22 +0300

    Add BusinessInfoDto class with relevant properties

---
+++ PartneroBackend/Models/DTOs/BusinessInfoDto.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.Runtime.EventStreams;
+using MongoDB.Bson;
+
+namespace PartneroBackend.Models.DTOs
+{
+    public class BusinessInfoDto
+    {
+        public ObjectId Id { get; set; }
+        public string BusinessName { get; set; } = string.Empty;
+        public string AuthorName { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public List<string> ImageUrls { get; set; } = [];
+        public List<string> Offers { get; set; } = [];
+    }
+}
