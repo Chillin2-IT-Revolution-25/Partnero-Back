aa783804d674754a254b17ffc84f35a09f91e64d
valtosw <volodijbessarabov@knu.ua>
2025-06-01 09:25:34 +0300

    Add ApplicationUserController for business info retrieval

---
+++ PartneroBackend/Controllers/ApplicationUserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using PartneroBackend.Data;
+using PartneroBackend.Models.DTOs;
+using PartneroBackend.Models.InfoClasses;
+using PartneroBackend.Services;
+
+namespace PartneroBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController(MongoDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        [Route("business/{id}")]
+        public async Task<BusinessInfo?> GetBusinessById(string id)
+        {
+            if (!ObjectId.TryParse(id, out var objectId))
+            {
+                return null;
+            }
+
+            var business = await context.BusinessInfos.Find(b => b.Id == objectId).FirstOrDefaultAsync();
+
+            return business;
+        }
+    }
+}
