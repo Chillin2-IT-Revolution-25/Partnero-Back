6d387ee1e6b4c7cbbd984f6d9b1920e2ee005a82
valtosw <volodijbessarabov@knu.ua>
2025-06-01 04:03:06 +0300

    Add PersonalInfo class with user profile properties

---
+++ PartneroBackend/Models/InfoClasses/PersonalInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace PartneroBackend.Models.InfoClasses
+{
+    public class PersonalInfo
+    {
+        public string? ProfilePictureUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string? Website { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public string? Bio { get; set; }
+    }
+}
