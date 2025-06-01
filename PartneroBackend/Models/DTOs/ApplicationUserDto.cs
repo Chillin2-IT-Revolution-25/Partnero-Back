e0cddccbbcdedcfc3fd0329fac5e19acacc62aa7
valtosw <volodijbessarabov@knu.ua>
2025-06-01 09:24:32 +0300

    Add ApplicationUserDto and UserBusinessInfoDto classes

---
+++ PartneroBackend/Models/DTOs/ApplicationUserDto.cs
@@ -0,0 +1,23 @@
+﻿using PartneroBackend.Models.InfoClasses;
+
+namespace PartneroBackend.Models.DTOs
+{
+    public class ApplicationUserDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string AvatarUrl { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+        public UserBusinessInfoDto? BusinessInfo { get; set; }
+    }
+
+    public class UserBusinessInfoDto
+    {
+        public string? Name { get;set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public string? Category { get; set; }
+    }
+}
