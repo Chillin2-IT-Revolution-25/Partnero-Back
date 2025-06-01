4a3286933bc428663477f651ee3ea11a7b46e035
valtosw <volodijbessarabov@knu.ua>
2025-06-01 04:02:40 +0300

    Add SocialMediaInfo class for social media properties

---
+++ PartneroBackend/Models/InfoClasses/SocialMediaInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace PartneroBackend.Models.InfoClasses
+{
+    public class SocialMediaInfo
+    {
+        public string? Instagram { get; set; }
+        public string? YouTube { get; set; }
+        public string? LinkedIn { get; set; }
+        public string? Website { get; set; }
+    }
+}
