be77a8594397cd721e82c353c446fa24ac34221a
valtosw <volodijbessarabov@knu.ua>
2025-06-01 04:03:43 +0300

    Add MongoDbContext for database connection management

---
+++ PartneroBackend/Data/MongoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using PartneroBackend.Models.InfoClasses;
+
+namespace PartneroBackend.Data
+{
+    public class MongoDbContext
+    {
+        public IMongoCollection<BusinessInfo> BusinessInfos { get; }
+
+        public MongoDbContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("MongoDbConnectionString"));
+            var database = client.GetDatabase("Partnero");
+
+            BusinessInfos = database.GetCollection<BusinessInfo>("BusinessInfos");
+
+        }
+    }
+}
