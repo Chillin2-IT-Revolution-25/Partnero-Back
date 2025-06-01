125e9ce8d382ca3e44f3fb3671dd65a5b97ab82b
valtosw <volodijbessarabov@knu.ua>
2025-06-01 09:24:50 +0300

    Refactor LoginResponse to include ApplicationUserDto

--- PartneroBackend/Models/DTOs/AuthDTOs/LoginResponse.cs
+++ PartneroBackend/Models/DTOs/AuthDTOs/LoginResponse.cs
@@ -3,8 +3,6 @@
     public class LoginResponse
     {
         public bool Success { get; set; }
-        public string AccessToken { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string UserId { get; set; } = string.Empty;
+        public ApplicationUserDto User { get; set; } = new ApplicationUserDto();
     }
 }
